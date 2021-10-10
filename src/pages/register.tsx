86acb4d179071806c28e277b089a36fd97811b28
Arpit Bansal <arpit.bansal@outlook.com>
2021-10-10 14:42:11 +0530

    Register form WIP

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import React from 'react';
 import {
     FormErrorMessage,
@@ -9,43 +9,39 @@ import {
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 
-interface registerProps {
+interface RegisterProps {
     firstName: string;
     lastName: string;
     email: string;
     password: string;
     confirmPassword: string;
 }
-export const Register: React.FC<registerProps> = ({ }) => {
+export const Register: React.FC<RegisterProps> = ({ }) => {
 
     const {
         handleSubmit,
         register,
         formState: { errors, isSubmitting }
-    } = useForm();
-    function onSubmit(values) {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                resolve('completed');
-            }, 3000);
-        });
-    }
+    } = useForm<RegisterProps>();
+    const onSubmit: SubmitHandler<RegisterProps> = (data: any) => {
+        console.log(data);
+        console.log('inside the oinSubmit form handler');
+      };
     return (
         <Wrapper variant="small">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl isInvalid={errors.name}>
+                <FormControl isInvalid={errors.firstName}>
                     <FormLabel htmlFor="name">First name</FormLabel>
                     <Input
-                        id="userName"
-                        placeholder="Username"
-                        {...register("username", {
+                        id="firstName"
+                        placeholder="First Name"
+                        {...register("firstName", {
                             required: "This is required",
                             minLength: { value: 4, message: "Minimum length should be 4" }
                         })}
                     />
                     <FormErrorMessage>
-                        {errors.name && errors.name.message}
+                        {errors.firstName && errors.firstName.message}
                     </FormErrorMessage>
                 </FormControl>
                 <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">

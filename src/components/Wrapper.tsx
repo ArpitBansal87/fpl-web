4f2f2892406526fb1e648eb8c05a0d8ac9874c8d
Arpit Bansal <arpit.bansal@outlook.com>
2021-10-22 02:57:30 +0530

    Changes for register flow

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,10 +1,15 @@
-import { Box, Container } from '@chakra-ui/layout';
+import { Box, Container, Center } from '@chakra-ui/layout';
 import React from 'react'
 
 interface WrapperProps {
     variant?: 'small' | 'regular'
 }
 export const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => {
-    return (<Container maxW="container.lg">
-        <Box padding="4" bg="gray.100" maxW={variant === "regular" ? "3xl" : "xl"}>{children}</Box></Container>);
+    return (<Container maxW="container.lg" style={{ height: "100vh" }}>
+        <Center h="100%">
+            <Box padding="4" bg="gray.100" maxW={variant === "regular" ? "3xl" : "2xl"}>
+                {children}
+            </Box>
+        </Center>
+    </Container>);
 }
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,47 +1,156 @@
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import React from 'react';
+import React, { useState } from 'react';
 import {
     FormErrorMessage,
     FormLabel,
     FormControl,
     Input,
-    Button
+    Button,
+    InputGroup,
+    InputRightElement,
+    useToast,
+    Center,
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
+import { useEffect } from "react";
+import { useMutation } from "urql";
+import _ from "lodash";
 
 interface RegisterProps {
-    firstName: string;
-    lastName: string;
+    username: string;
     email: string;
     password: string;
     confirmPassword: string;
 }
+
+interface RegisterInput {
+    "username": string,
+    "password": string,
+    "confirmPassword": string,
+    "email": string
+}
+
+const REGISTER_MUTATION = `
+mutation RegisterMutation($registerInput: RegisterInput) {
+    register(registerInput: $registerInput) {
+      createdAt
+      id
+      username
+    }
+  }
+  `;
+
 export const Register: React.FC<RegisterProps> = ({ }) => {
 
+    const [{ }, registerFunction] = useMutation(REGISTER_MUTATION);
+    const toast = useToast();
     const {
         handleSubmit,
         register,
+        setError,
         formState: { errors, isSubmitting }
     } = useForm<RegisterProps>();
-    const onSubmit: SubmitHandler<RegisterProps> = (data: any) => {
-        console.log(data);
-        console.log('inside the oinSubmit form handler');
-      };
+    const onSubmit: SubmitHandler<RegisterProps> = async (data: any) => {
+        setMutationError({});
+        await registerFunction({ registerInput: data as RegisterInput }).then(response => {
+            if (response.error) {
+                const graphQLError = _.head(response?.error?.graphQLErrors);
+                if (graphQLError) setMutationError(graphQLError?.extensions?.errors);
+            }
+            else if (response.data) {
+                const responseObj = response?.data?.register;
+                toast({
+                    title: "Account created.",
+                    description: "We've created your account for you.",
+                    status: "success",
+                    duration: 9000,
+                    isClosable: true,
+                })
+            }
+        })
+    };
+    const [mutationError, setMutationError] = useState({ username: '', confirmPassword: '', password: '', email: '' });
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const handlePasswordToggle = () => setShowPassword(!showPassword);
+    const handleConfirmPasswordToggle = () => setShowConfirmPassword(!showConfirmPassword);
+
     return (
         <Wrapper variant="small">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl isInvalid={errors.firstName}>
-                    <FormLabel htmlFor="name">First name</FormLabel>
+                <FormControl isInvalid={!!errors.username || mutationError && !_.isEmpty(mutationError.username)}>
+                    <FormLabel htmlFor="name">User name</FormLabel>
                     <Input
-                        id="firstName"
-                        placeholder="First Name"
-                        {...register("firstName", {
+                        id="username"
+                        placeholder="User Name"
+                        {...register("username", {
                             required: "This is required",
                             minLength: { value: 4, message: "Minimum length should be 4" }
                         })}
                     />
                     <FormErrorMessage>
-                        {errors.firstName && errors.firstName.message}
+                        {(errors.username && errors.username.message) || (mutationError && mutationError.username)}
+                    </FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.email || (mutationError && !_.isEmpty(mutationError.email))}>
+                    <FormLabel htmlFor="name">Email</FormLabel>
+                    <Input
+                        id="email"
+                        placeholder="Email"
+                        {...register("email", {
+                            required: "This is required",
+                            minLength: { value: 4, message: "Minimum length should be 4" }
+                        })}
+                        type="email"
+                    />
+                    <FormErrorMessage>
+                        {errors.email && errors.email.message}{mutationError && mutationError.email}
+                    </FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.password || (mutationError && !_.isEmpty(mutationError.password))}>
+                    <FormLabel htmlFor="name">Password</FormLabel>
+                    <InputGroup size="md">
+                        <Input
+                            pr="4.5rem"
+                            type={showPassword ? "text" : "password"}
+                            id="password"
+                            placeholder="Password"
+                            {...register("password", {
+                                required: "This is required",
+                                minLength: { value: 4, message: "Minimum length should be 4" }
+                            })}
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" onClick={handlePasswordToggle}>
+                                {showPassword ? "Hide" : "Show"}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <FormErrorMessage>
+                        {errors.password && errors.password.message}{mutationError && mutationError.password}
+                    </FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.confirmPassword || (mutationError && !_.isEmpty(mutationError.confirmPassword))}>
+                    <FormLabel htmlFor="name">Confirm Password</FormLabel>
+                    <InputGroup size="md">
+                        <Input
+                            pr="4.5rem"
+                            type={showConfirmPassword ? "text" : "password"}
+                            id="confirmPassword"
+                            placeholder="Confirm Password"
+                            {...register("confirmPassword", {
+                                required: "This is required",
+                                minLength: { value: 4, message: "Minimum length should be 4" },
+                            })}
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" onClick={handleConfirmPasswordToggle}>
+                                {showConfirmPassword ? "Hide" : "Show"}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <FormErrorMessage>
+                        {errors.confirmPassword && errors.confirmPassword.message}{mutationError && mutationError.confirmPassword}
                     </FormErrorMessage>
                 </FormControl>
                 <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
